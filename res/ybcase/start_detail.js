1c09164c46b11db550a2385a7be922cc753e1393
wangqizhi_centos <wangqizhi1987@gmail.com>
2014-03-04 18:12:34 +0800

    yb_case fix input empty

--- res/ybcase/start_detail.js
+++ res/ybcase/start_detail.js
@@ -4,6 +4,10 @@ $('#start_flow_btn').on('click',function(){
 	var all_temp_size = $('#size_val').val();
 	var email_who = $('#temp_val_email').val();
 	var flow_name = $('#flow_name_val').val();
+	if ($('.temp_value').val() =='') {
+		alertify.error('Emptp Input');
+		return false;
+	};
 	$('.temp_value').each(function(){
 		all_temp_val += $(this).attr('id')+':'+$(this).val()+'-&*&-';
 		// all_temp_val.$(this).attr('id') = $(this).val();
